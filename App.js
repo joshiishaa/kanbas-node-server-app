496954432166ef4b9103e0adbbd818674ecbed35
joshiishaa <joshiishaa@gmail.com>
2024-04-16 13:25:43 -0400

    a6 node.js

--- App.js
+++ App.js
@@ -1,16 +1,46 @@
 import express from 'express'
+import session from "express-session";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js"
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import "dotenv/config";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'||
+'mongodb+srv://joshiishaa:Hello!23@cluster0.itacrhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(CONNECTION_STRING);
 
 
+
+//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
-app.use(express.json());
-ModuleRoutes(app);
+app.use(
+  cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+  })
+);
+ const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 
+  
+app.use(express.json());
+UserRoutes(app);
+ModuleRoutes(app);
 CourseRoutes(app);
 Hello(app)
 Lab5(app)
